ba84761142a72a835ddf2431e0c1c98364358c7d
jreyesFoodin <juan.reyes@foodin.mx>
2024-01-13 16:31:08 -0500

    [add] se agrego actiones para peticiones de blog

---
+++ src/action/blog.js
@@ -0,0 +1,49 @@
+import useApi from '../hooks/useApi'
+
+export const useBlog = () => {
+    const api = useApi()
+    const AllListBlogAction = async () => {
+      try {
+        const response = await api.get('Blog/list', null, null)
+        return response
+      } catch (error) {
+        console.log('Error AllListBlog', error.data.response)
+        return error
+      }
+    }
+    const listBlogByIdAction = async (id) => {
+      try {
+        const headers = { id }
+        const response = await api.get('Blog/listBlogById', null, headers)
+        return response
+      } catch (error) {
+        console.log('Error listBlogByIdAction', error.data.response)
+        return error
+      }
+    }
+    const CreatedBlogAction = async (body) => {
+      try {
+        const response = await api.post('Blog/createdBlog', body, null)
+        return response
+      } catch (error) {
+        console.log('Error CreatedBlogAction', error.data.response)
+        return error
+      }
+    }
+    const UpdateBlogAction = async (body, id) => {
+      try {
+        const headers = { id }
+        const response = await api.put('Blog/updateBlog', body, null, headers)
+        return response
+      } catch (error) {
+        console.log('Error UpdateBlogAction', error.data.response)
+        return error
+      }
+    }
+    return {
+      AllListBlogAction,
+      listBlogByIdAction,
+      CreatedBlogAction,
+      UpdateBlogAction
+    }
+  }
